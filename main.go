3a3db89e1013d47ca33afe509b2c9be58f41931c
Takamichi Omori <merrytakamerry@gmail.com>
2020-12-14 23:02:25 +0900

    dockernized

--- main.go
+++ main.go
@@ -2,16 +2,54 @@ package main
 
 import (
     "fmt"
+    "github.com/gorilla/websocket"
     "log"
     "net/http"
 )
 
+// We'll need to define an Upgrader
+// this will require a Read and Write buffer size
+var upgrader = websocket.Upgrader{
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+}
+
+func reader(conn *websocket.Conn) {
+    for {
+        messageType, p, err := conn.ReadMessage()
+        if err != nil {
+            log.Println(err)
+            return
+        }
+
+        log.Println(string(p))
+
+        if err := conn.WriteMessage(messageType, p); err != nil {
+            log.Println(err)
+            return
+        }
+    }
+}
 func homePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Home Page")
+    fmt.Fprintf(w, "please connect via WebSocket")
 }
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World")
+    upgrader.CheckOrigin = func(r *http.Request) bool {
+        return true
+    }
+
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+    }
+    log.Println("Client Connected")
+    err = ws.WriteMessage(1, []byte("Hi Client!"))
+    if err != nil {
+        log.Println(err)
+    }
+    reader(ws)
+    log.Println("Client Disconnected")
 }
 
 func setupRoutes() {
@@ -22,5 +60,5 @@ func setupRoutes() {
 func main() {
     fmt.Println("Hello World")
     setupRoutes()
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Fatal(http.ListenAndServe(":80", nil))
 }
