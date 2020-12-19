54aa5255550dafaa19fbccb9a2766d8bb93ad4a9
Takamichi Omori <merrytakamerry@gmail.com>
2020-12-19 15:05:35 +0900

    :sparkles: choose current Num

--- kubesmas-tree/handler.go
+++ kubesmas-tree/handler.go
@@ -130,9 +130,14 @@ func getCurrentValues() (*GETResponse, error) {
 		return nil, errors.Wrap(err, "failed to getPods()")
 	}
 
+	cip, err := getValue("CURRENT_ILLUMINATION_PATTERN")
+	if err != nil {
+		return nil, err
+	}
+
 	r := GETResponse{
 		Pods:                len(pods.Items),
-		IlluminationPattern: rand.Intn(4),
+		IlluminationPattern: cip,
 		IlluminationData:    *id,
 	}
 	return &r, nil
@@ -226,6 +231,7 @@ func addValue(target string) error {
 	}
 	return nil
 }
+
 func declValue(target string) (int, error){
 	redisPath := os.Getenv("REDIS_PATH")
 	client, err := redis.New(redisPath)
